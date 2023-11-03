3df84e844e9ac5a203a365dc6f88f8c2aad01bd8
Lefia <lefia1222@gmail.com>
2023-11-04 01:33:24 +0800

    💄 Apply hover animation to menuItem

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -21,8 +21,8 @@ export default function MenuItem(props: MenuItemProps) {
         'flex items-center justify-center',
         'transition-all',
         {
-          'bg-teal-600 text-zinc-100 scale-110 my-6': active,
-          'bg-zinc-800 text-zinc-100': !active,
+          'bg-teal-600 text-zinc-100 scale-[1.3] my-6 hover:scale-[1.5]': active,
+          'bg-zinc-800 text-zinc-100 hover:scale-[1.2]': !active,
         }
       )}
       onClick={handleClick}
