18f4dbc4901158eb8fb2aacd34596a860098beb0
Lefia <lefia1222@gmail.com>
2023-11-06 00:43:04 +0800

    ✨ Add population data of ROC 102

--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -4,12 +4,18 @@ import * as d3 from 'd3'
 
 
 export default function Bar() {
-  const [data, setData] = useState<d3.DSVRowArray<string>>()
+  const [data1, setData1] = useState<d3.DSVRowArray<string>>()
+  const [data2, setData2] = useState<d3.DSVRowArray<string>>()
 
   useEffect(() => {
-    // https://data.gov.tw/dataset/161717
+    // 102年: https://data.gov.tw/dataset/153086
+    d3.csv(`${import.meta.env.BASE_URL}data/102年現住人口數按性別及單齡人口分.csv`).then((data) => {
+      setData1(data)
+    })
+
+    // 112年: https://data.gov.tw/dataset/161717
     d3.csv(`${import.meta.env.BASE_URL}data/11208年齡.csv`).then((data) => {
-      setData(data)
+      setData2(data)
     })
   }, [])
 
@@ -17,22 +23,40 @@ export default function Bar() {
     <Plot
       data={[
         {
-          y: data?.columns.slice(3).map((age) => {
-            return data.reduce((acc, cur) => {
+          name: '102年',
+          y: data1?.columns.slice(3).map((age) => {
+            return data1.reduce((acc, cur) => {
+              return acc + parseInt(cur[age].replace(/,/g, "").trim())
+            }, 0)
+          }),
+          type: 'bar',
+          marker: {
+            color: '#ff7f0e',
+          }
+        },
+        {
+          name: '112年',
+          y: data2?.columns.slice(3).map((age) => {
+            return data2.reduce((acc, cur) => {
               return acc + parseInt(cur[age])
             }, 0)
           }),
           type: 'bar',
+          marker: {
+            color: '#1f77b4',
+          }
         },
+        
       ]}
       layout={{
-        title: '桃園市112年各區人口年齡層統計',
+        title: '桃園市人口年齡層分布: 102 vs 112 年',
         xaxis: {
           title: '年齡(歲)',
         },
         yaxis: {
           title: '人口數(人)',
         },
+        barmode: 'group',
         autosize: true,
         paper_bgcolor: 'rgba(0,0,0,0)',
         modebar: {
