29b01082c7b3bfdf1689d584381a8ec75d261b17
Lefia <lefia1222@gmail.com>
2023-11-06 22:36:05 +0800

    ✨ Update the Bar chart

--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -4,18 +4,29 @@ import * as d3 from 'd3'
 
 
 export default function Bar() {
-  const [data1, setData1] = useState<d3.DSVRowArray<string>>()
-  const [data2, setData2] = useState<d3.DSVRowArray<string>>()
+  const [data1, setData1] = useState<number[]>([])
+  const [data2, setData2] = useState<number[]>()
+
 
   useEffect(() => {
     // 102年: https://data.gov.tw/dataset/153086
     d3.csv(`${import.meta.env.BASE_URL}data/102年現住人口數按性別及單齡人口分.csv`).then((data) => {
-      setData1(data)
+      const populationByAge = data.columns.slice(3).map((age) => {
+        return data.reduce((acc, cur) => {
+          return acc + parseInt(cur[age].replace(/,/g, "").trim())
+        }, 0)
+      })
+      setData1(populationByAge)
     })
 
     // 112年: https://data.gov.tw/dataset/161717
     d3.csv(`${import.meta.env.BASE_URL}data/11208年齡.csv`).then((data) => {
-      setData2(data)
+      const populationByAge = data.columns.slice(3).map((age) => {
+        return data.reduce((acc, cur) => {
+          return acc + parseInt(cur[age].replace(/,/g, "").trim())
+        }, 0)
+      })
+      setData2(populationByAge)
     })
   }, [])
 
@@ -24,23 +35,25 @@ export default function Bar() {
       data={[
         {
           name: '102年',
-          y: data1?.columns.slice(3).map((age) => {
-            return data1.reduce((acc, cur) => {
-              return acc + parseInt(cur[age].replace(/,/g, "").trim())
-            }, 0)
-          }),
+          y: data1,
           type: 'bar',
           marker: {
             color: '#ff7f0e',
           }
         },
         {
+          name: '102年(年齡+10)',
+          x: Array.from({ length: 101 }, (_, index) => index + 10),
+          y: data1.slice(0, 91),
+          type: 'bar',
+          marker: {
+            color: '#ff7f0e',
+          },
+          visible: false,
+        },
+        {
           name: '112年',
-          y: data2?.columns.slice(3).map((age) => {
-            return data2.reduce((acc, cur) => {
-              return acc + parseInt(cur[age])
-            }, 0)
-          }),
+          y: data2,
           type: 'bar',
           marker: {
             color: '#1f77b4',
@@ -49,7 +62,7 @@ export default function Bar() {
         
       ]}
       layout={{
-        title: '桃園市人口年齡層分布: 102 vs 112 年',
+        title: '桃園市人口年齡層分布: 102年 & 112 年',
         xaxis: {
           title: '年齡(歲)',
         },
@@ -57,6 +70,24 @@ export default function Bar() {
           title: '人口數(人)',
         },
         barmode: 'group',
+        updatemenus: [{
+          buttons: [
+            {
+              args: [{ 'visible': [true, false, true] }],
+              label: '圖一',
+            },
+            {
+              args: [{ 'visible': [false, true, true] }],
+              label: '圖二',
+            },
+          ],
+          type: 'buttons',
+          direction: 'left',
+          xanchor: 'left',
+          x: -0.05,
+          yanchor: 'top',
+          y: 1.15,
+        }],
         autosize: true,
         paper_bgcolor: 'rgba(0,0,0,0)',
         modebar: {
